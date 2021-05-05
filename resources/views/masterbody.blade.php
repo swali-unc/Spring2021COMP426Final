80e301547011a4831fb484c56c8f16e810ea40f1
Syed Ali <Syed.Waqqas.Ali@gmail.com>
2021-05-04 22:18:47 -0400

    add header and login/logout/register links

--- resources/views/masterbody.blade.php
+++ resources/views/masterbody.blade.php
@@ -5,10 +5,21 @@
 @endpush
 
 @section('Body')
+<table class="maxwidth center centertext">
+	<tr>
+		<td>A.I. Tic-Tac-Toe</td>
+	</tr>
+</table>
+
 <table class="maxwidth">
 	<tr>
 		<td><a href="/">Home</a></td>
-		<td></td>
+		@if($IsLoggedIn)
+		<td><a href="/logout">Logout</a></td>
+		@else
+		<td><a href="/login">Login</a></td>
+		<td><a href="/register">Register</a></td>
+		@endif
 	</tr>
 </table>
 @yield('PageContents')
