a7cc96bdfd8f07012ce1b8d5859cead028f8351b
Syed Ali <Syed.Waqqas.Ali@gmail.com>
2021-05-04 21:34:01 -0400

    session middleware proxies the login token and sets variables

---
+++ app/Http/Middleware/SessionMiddleware.php
@@ -0,0 +1,27 @@
+<?
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Library\SessionAuth;
+use Illuminate\Support\Facades\View;
+
+class SessionMiddleware {
+	protected $auth;
+	
+	public function handle( Request $request, Closure $next ) {
+		if( $this->auth->CheckSessionWithRequest( $request ) ) {
+			View::share('IsLoggedIn', true );
+			View::share('username', $this->auth->GetUsername() );
+		} else {
+			View::share('IsLoggedIn', false );
+		}
+		
+		return $next( $request );
+	}
+	
+	public function __construct() {
+		$auth = SessionAuth::Instance();
+	}
+}
