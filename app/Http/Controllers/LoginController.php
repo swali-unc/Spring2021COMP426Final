e09098a1414848e972eaf46341ae0baa6ec5154a
Syed Ali <Syed.Waqqas.Ali@gmail.com>
2021-05-05 15:19:00 -0400

    Add controller actions for registration

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -20,6 +20,19 @@ class LoginController {
 		return view('register');
 	}
 	
+	public function CreateUser( Request $request ) {
+		$this->validate( $request, [
+				'username' => 'required|alpha|max:50|min:3',
+				'password' => 'required|max:50|min:3',
+		]);
+		
+		$username = $request->input( 'username' );
+		$password = $request->input( 'password' );
+		
+		$result = $this->auth->CreateUser( $username, $password );
+		return $result ? redirect('/login')->with('errormsg','Account created, please log in') : view('register',['errormsg'=>'Username not allowed or already taken']);
+	}
+	
 	public function LogoutPage( Request $request ) {
 		$this->auth->CloseSession();
 		$this->auth->RemoveSessionCookie();
@@ -38,7 +51,7 @@ class LoginController {
 		list( $success, $authresult ) = $this->auth->CheckCredentials( $username, $password );
 		
 		if( $success !== true )
-			return view('login',['loginerror'=>$authresult]);
+			return view('login',['errormsg'=>$authresult]);
 		list( $cookiename, $token ) = $this->auth->CreateSession( $authresult );
 		return redirect( '/' )
 			->withCookie( $cookiename, $token, 20160 );
