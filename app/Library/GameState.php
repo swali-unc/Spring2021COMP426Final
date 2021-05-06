1565cc83d28c6d64216cf32220a2fb3555d465b1
Syed Ali <Syed.Waqqas.Ali@gmail.com>
2021-05-06 04:48:39 -0400

    add ability to track game states

--- app/Library/GameState.php
+++ app/Library/GameState.php
@@ -4,10 +4,15 @@ namespace App\Library;
 
 use DB;
 use App\Library\SessionAuth;
+use App\Library\ScoreSystem;
 
 class GameState {
-	private $playerid;
-	private $gamerow;
+	private $playerid = null;
+	private $gameid = null;
+	private $gamerow = null;
+	private $indexes = ['tl','tr','tm','cl','cm','cr','bl','bm','br'];
+	private $winner = null; // -1 for enemy, 0 for none, 1 for player, 2 for draw
+	private $fightid = null;
 	
 	public function IsInGame() {
 		return $this->gamerow !== null;
@@ -15,6 +20,97 @@ class GameState {
 	
 	public function GetGameRow() {
 		return $this->gamerow;
+	}
+	
+	public function MakeMove( $index, $value ) {
+		if( !$this->IsValidIndex( $index ) ) return false;
+		if( !$this->IsValidValue( $value ) ) return false;
+		DB::table('games')
+			->where('id',$this->gameid)
+			->update([$index => $value]);
+		return true;
+	}
+	
+	public function IsMoveValid( $index ) {
+		return $this->IsValidIndex( $index ) && $this->gamerow->$index == 0;
+	}
+	
+	public function IsDraw() {
+		return $this->winner == 2;
+	}
+	
+	public function IsWon() {
+		return $this->winner == 1;
+	}
+	
+	public function IsLost() {
+		return $this->winner == -1;
+	}
+	
+	public function IsInProgress() {
+		return $this->winner == 0;
+	}
+	
+	public function IsGameCompleted() {
+		// check for 3 in cols, rows, or diagonals
+		// this is ugly, but whatever, tic tac toe doesn't need to scale, and if it does,
+		// this can be easily rewritten with arrays
+		if( $this->gamerow->tl == $this->gamerow->tm && $this->gamerow->tm == $this->gamerow->tr && $this->gamerow->tr != 0 ) {
+			$this->winner = $this->gamerow->tl;
+			return true;
+		}
+		if( $this->gamerow->cl == $this->gamerow->cm && $this->gamerow->cm == $this->gamerow->cr && $this->gamerow->cr != 0 ) {
+			$this->winner = $this->gamerow->cl;
+			return true;
+		}
+		if( $this->gamerow->bl == $this->gamerow->bm && $this->gamerow->bm == $this->gamerow->br && $this->gamerow->br != 0 ) {
+			$this->winner = $this->gamerow->bl;
+			return true;
+		}
+		if( $this->gamerow->tl == $this->gamerow->cm && $this->gamerow->cm == $this->gamerow->br && $this->gamerow->br != 0 ) {
+			$this->winner = $this->gamerow->br;
+			return true;
+		}
+		if( $this->gamerow->tr == $this->gamerow->cm && $this->gamerow->cm == $this->gamerow->bl && $this->gamerow->bl != 0 ) {
+			$this->winner = $this->gamerow->bl;
+			return true;
+		}
+		
+		foreach( $this->indexes as $i ) {
+			if( $this->gamerow->$i == 0 )
+				return false;
+		}
+		
+		$this->winner = 2;
+		return true;
+	}
+	
+	public function CompleteGame() {
+		if( $this->winner == 0 ) return false;
+		$score = $this->winner == -1 ? 0 : mt_rand(10,50);
+		ScoreSystem::Instance()->RecordGame( $this->playerid, $this->fightid, $score );
+		
+		DB::table('games')
+			->where('id',$this->gameid)
+			->delete();
+	}
+	
+	public function CreateGame( $fightid ) {
+		if( $this->IsInGame() ) return false;
+		DB::table('games')
+			->insert([
+				'userid' => $this->playerid,
+				'fightid' => $fightid
+			]);
+		return true;
+	}
+	
+	private function IsValidIndex( $index ) {
+		return in_array( $index, $this->indexes, true );
+	}
+	
+	private function IsValidValue( $value ) {
+		return in_array( $value, [1,0,-1], true );
 	}
 	
 	public function __construct( $playerid = -1 ) {
@@ -32,10 +128,13 @@ class GameState {
 			->get();
 		
 		if( count( $rows ) == 0 ) {
-			$gamerow = null;
+			$this->gamerow = null;
 			return;
 		}
 		
 		$this->gamerow = $rows[0];
+		$this->gameid = $this->gamerow->id;
+		$this->fightid = $this->gamerow->fightid;
+		$this->IsGameCompleted();
 	}
 }
