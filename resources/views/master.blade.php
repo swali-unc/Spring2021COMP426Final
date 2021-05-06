0e4c41686ebe6b8b62fe348e3fe7f0c311a54e71
Syed Ali <Syed.Waqqas.Ali@gmail.com>
2021-05-06 03:06:05 -0400

    cache prevention

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,6 +3,15 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Prevent cache as pages dynamically change  -->
+        <meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="cache-control" content="no-store" />
+		<meta http-equiv="cache-control" content="must-revalidate" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
 
         <title>@yield('PageTitle')</title>
 
