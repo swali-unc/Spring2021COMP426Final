b7b0e58f441d5f7f66813bfa995a2611c743bea2
Syed Ali <Syed.Waqqas.Ali@gmail.com>
2021-05-06 13:47:54 -0400

    changing looks of fight select

--- resources/views/fightselect.blade.php
+++ resources/views/fightselect.blade.php
@@ -9,7 +9,7 @@
 		<div class="row">
 		@endif
 			<div class="col3">
-				<fieldset class="fsheader vh25">
+				<fieldset class="fsheader vh23">
 					<legend>{!! $fight['name'] !!}</legend>
 					<img src="{{ $fight['logo'] }}" class="floatleft fighterimg" />
 					<p class="fighterquote">
