ed4fec9a8e5b253e8c2fb59718f089a1cdf30bd9
Syed Ali <Syed.Waqqas.Ali@gmail.com>
2021-05-05 23:50:39 -0400

    added text content to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,14 @@
 @section('PageTitle','Home Page')
 
 @section('PageContents')
-Hello World
+<fieldset class="fsheader">
+	<p>Welcome to AI Tic-Tac-Toe, where you can challenge several AI, compete for a high score list, and participate in all things competitive tic-tac-toe.<br /></p>
+@if( $IsLoggedIn )
+	<p>Start playing by clicking the <a href="/play">play</a> button!</p>
+@else
+	<p>If you wish to play, please <a href="/register">register</a> or <a href="/login">log in</a></p>
+@endif
+</fieldset>
+<fieldset class="fsfooter">
+</fieldset>
 @endsection
