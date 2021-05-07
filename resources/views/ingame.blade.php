5f1d75fc7c14c5cbc4d406fd082ba30aeed31812
Syed Ali <Syed.Waqqas.Ali@gmail.com>
2021-05-06 23:06:51 -0400

    loaders were not showing up in tic tac toe board

--- resources/views/ingame.blade.php
+++ resources/views/ingame.blade.php
@@ -24,19 +24,19 @@
 		<td class="width50">
 			<table class="gridtable">
 				<tr>
-					<td id="tl" class="gridelement borderright borderbottom" onclick="window.ClickGrid('tl');"><div id="loader"></div></td>
-					<td id="tm" class="gridelement borderleft borderright borderbottom" onclick="window.ClickGrid('tm');"><div id="loader"></div></td>
-					<td id="tr" class="gridelement borderleft borderbottom" onclick="window.ClickGrid('tr');"><div id="loader"></div></td>
+					<td id="tl" class="gridelement borderright borderbottom" onclick="window.ClickGrid('tl');"><div class="loader"></div></td>
+					<td id="tm" class="gridelement borderleft borderright borderbottom" onclick="window.ClickGrid('tm');"><div class="loader"></div></td>
+					<td id="tr" class="gridelement borderleft borderbottom" onclick="window.ClickGrid('tr');"><div class="loader"></div></td>
 				</tr>
 				<tr>
-					<td id="cl" class="gridelement bordertop borderright borderbottom" onclick="window.ClickGrid('cl');"><div id="loader"></div></td>
-					<td id="cm" class="gridelement bordertop borderbottom borderleft borderright" onclick="window.ClickGrid('cm');"><div id="loader"></div></td>
-					<td id="cr" class="gridelement borderleft bordertop borderbottom" onclick="window.ClickGrid('cr');"><div id="loader"></div></td>
+					<td id="cl" class="gridelement bordertop borderright borderbottom" onclick="window.ClickGrid('cl');"><div class="loader"></div></td>
+					<td id="cm" class="gridelement bordertop borderbottom borderleft borderright" onclick="window.ClickGrid('cm');"><div class="loader"></div></td>
+					<td id="cr" class="gridelement borderleft bordertop borderbottom" onclick="window.ClickGrid('cr');"><div class="loader"></div></td>
 				</tr>
 				<tr>
-					<td id="bl" class="gridelement bordertop borderright" onclick="window.ClickGrid('bl');"><div id="loader"></div></td>
-					<td id="bm" class="gridelement bordertop borderleft borderright" onclick="window.ClickGrid('bm');"><div id="loader"></div></td>
-					<td id="br" class="gridelement borderleft bordertop" onclick="window.ClickGrid('br');"><div id="loader"></div></td>
+					<td id="bl" class="gridelement bordertop borderright" onclick="window.ClickGrid('bl');"><div class="loader"></div></td>
+					<td id="bm" class="gridelement bordertop borderleft borderright" onclick="window.ClickGrid('bm');"><div class="loader"></div></td>
+					<td id="br" class="gridelement borderleft bordertop" onclick="window.ClickGrid('br');"><div class="loader"></div></td>
 				</tr>
 			</table>
 		</td>
